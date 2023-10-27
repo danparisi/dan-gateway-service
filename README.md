80ecb73d226e36454122ad8e76a93c9972569e9f
Daniele Parisi <neb.inf@gmail.com>
2023-10-27 11:26:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dan-gateway-service
---
+++ src/test/java/com/danservice/gateway/ApplicationContextTest.java
@@ -0,0 +1,17 @@
+package com.danservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@SpringBootTest
+class ApplicationContextTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@TestConfiguration
+	public static class ApplicationTestConfiguration {
+	}
+}
